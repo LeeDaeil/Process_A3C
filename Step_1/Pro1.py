21f9b4fec4ae6b30fca207af67a3b000bf1114aa
gyeongminyonn <gyeongmin@chosun.kr>
2018-10-11 13:10:03 +0900

    테스트 업데이트

--- Step_1/Pro1.py
+++ Step_1/Pro1.py
@@ -16,6 +16,7 @@ class Pro1(multiprocessing.Process):    # multiprocessing 에서 process 를 상
             self.iter += 1
             # 2. 1초 대기
             sleep(3)
+            print("go")
 
     def update_mem(self):
         # 아직 해결을 못한 문제중 하나로 메모리를 업데이트를 시도 할 때 메모리의 오염 문제를
